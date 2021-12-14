136f9fb86fd7b41391e82e05ea56a1daac3bb8ec
ballpython-pied <77561001+ballpython-pied@users.noreply.github.com>
2021-12-13 17:46:47 -0800

    insert cool file
    a calculator i fail to make

---
+++ advancedcalc.py
@@ -0,0 +1,24 @@
+print('roses are red ')
+print('you mow grass')
+print('i shoved my dick in ur moms ass')
+choice = input('''
+Please select the type of operation you want to perform:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+num1 = float(input("you retarded spoiled brat enter a number:"))
+op = (input("Enter operator: "))
+num2 = float(input("you need to put in a seconds number you down syndrome haver:" ))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "_":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Listen here lil nigga faggot failed abortion retard down syndrome haver, that cant be an option. you need to choose a valid option trashbag")
